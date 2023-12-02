0d1dfd56b229abd63145242335429619a5298012
Audrey Inglish <you@example.com>
2023-12-01 19:23:23 -0700

    testing api

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.VisualBasic;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+builder.Services.AddCors();
+
+
+// Dictionary<string, List<String> = new();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/users/{userId}",
+    (int userId) => $"The user id is {userId}"
+);
+
+app.MapGet("/cheesecake", () => "Hello World!");
+
 
 app.Run();
