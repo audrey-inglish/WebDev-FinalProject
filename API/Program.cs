b5c43803494416613a71938d9c62506723cc00c1
Audrey Inglish <you@example.com>
2023-12-01 19:38:39 -0700

    reset api

--- API/Program.cs
+++ API/Program.cs
@@ -2,18 +2,8 @@ using Microsoft.VisualBasic;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-builder.Services.AddCors();
-
-
-// Dictionary<string, List<String> = new();
+//builder.Services.AddCors();
 
 app.MapGet("/", () => "Hello World!");
-
-// app.MapGet("/users/{userId}",
-//     (int userId) => $"The user id is {userId}"
-// );
-
-// app.MapGet("/cheesecake", () => "Hello World!");
-
 
 app.Run();
